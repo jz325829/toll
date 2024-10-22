839292c3671b9d3d8ac5ae9b440f96688e34fce9
jason <jz325829@gmail.com>
2024-10-22 19:39:39 +0800

    updated map

--- src/components/StreetPlan/index.tsx
+++ src/components/StreetPlan/index.tsx
@@ -31,21 +31,21 @@ const StreetPlan = ({
     <>
       {!isOpenPopUp && 
         <Box 
-        position="absolute" 
-        top="25px" 
-        right="55px" 
-        padding="5px"
-        paddingBottom="0px"
-        rounded={2}
-        zIndex={1000} 
-        cursor={'pointer'} 
-        backgroundColor="#ffffff">
-          <Image
-            src="images/street_plan.png"
-            alt="Chakra UI"
-            height="120px"
-            onClick={togglePopup}
-            />
+          position="absolute" 
+          top="25px" 
+          right="55px" 
+          padding="5px"
+          paddingBottom="0px"
+          rounded={2}
+          zIndex={1000} 
+          cursor={'pointer'} 
+          backgroundColor="#ffffff">
+            <Image
+              src="images/street_plan.png"
+              alt="Chakra UI"
+              height="120px"
+              onClick={togglePopup}
+              />
         </Box>
       }
       <Modal
@@ -55,106 +55,175 @@ const StreetPlan = ({
         width="100%"
         paddingTop="25px"
         id="street-plan-modal"
+        cursor={'default'}
       >
         <Image
           src="images/street_plan.png"
           width="100%"
           height="100%"
-          cursor={'pointer'}
           />
-        <Image
-          src="images/ellipse.png"
-          position="absolute" 
-          top="16%" 
-          right="51%"
-          onClick={() => togglePosition(1)}
-          cursor={'pointer'}
-          />
-        { currentPosition.id == 1 &&
+        { currentPosition.id == 1 ?
+          <>
+            <Image
+              src="images/ellipse.png"
+              position="absolute" 
+              top="16%" 
+              right="51%"
+              width="50px"
+              onClick={() => togglePosition(1)}
+            />
+            <Image
+              src="images/direction.png"
+              position="absolute"
+              top="19%"
+              right="50.6%"
+              width="60px"
+              transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
+              transformOrigin="top"
+              />
+          </>
+          :
           <Image
-            src="images/direction.png"
-            position="absolute"
-            top="18.5%"
-            right="50.5%"
-            transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
-            transformOrigin="top"
+            src="images/ellipse.png"
+            position="absolute" 
+            top="16%" 
+            right="51%"
+            onClick={() => togglePosition(1)}
+            cursor={'pointer'}
+            _hover={{ width: '50px', marginTop: '-6px', marginRight: '-6px'}}
+          />
+        }
+        { currentPosition.id == 3 ?
+          <>
+            <Image
+              objectFit="cover"
+              src="images/ellipse.png"
+              position="absolute" 
+              top="31.5%"
+              right="27%"
+              width="50px"
+              onClick={() => togglePosition(3)}
+              />
+            <Image
+              src="images/direction.png"
+              position="absolute"
+              top="34.5%"
+              right="27%"
+              width="50px"
+              transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
+              transformOrigin="top"
+              />
+          </>
+          :
+          <Image
+            objectFit="cover"
+            src="images/ellipse.png"
+            position="absolute" 
+            top="32%"
+            right="27%"
+            onClick={() => togglePosition(3)}
+            cursor={'pointer'}
+            _hover={{ width: '50px', marginTop: '-6px', marginRight: '-6px'}}
             />
         }
-        <Image
-          objectFit="cover"
-          src="images/ellipse.png"
-          position="absolute" 
-          top="32%"
-          right="27%"
-          onClick={() => togglePosition(3)}
-          cursor={'pointer'}
-          />
-        { currentPosition.id == 3 &&
-          <Image
-            src="images/direction.png"
-            position="absolute"
-            top="34.5%"
-            right="26.5%"
-            transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
-            transformOrigin="top"
+        { currentPosition.id == 2 ?
+          <>
+            <Image
+              objectFit="cover"
+              src="images/ellipse.png"
+              position="absolute" 
+              top="32%" 
+              right="73%"
+              width="50px"
+              onClick={() => togglePosition(2)}
+              />
+            <Image
+              src="images/direction.png"
+              position="absolute"
+              top="35%"
+              right="73%"
+              width="50px"
+              transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
+              transformOrigin="top"
+              />
+          </>
+          :
+            <Image
+              objectFit="cover"
+              src="images/ellipse.png"
+              position="absolute" 
+              top="32%" 
+              right="73%"
+              onClick={() => togglePosition(2)}
+              cursor={'pointer'}
+              _hover={{ width: '50px', marginTop: '-6px', marginRight: '-6px'}}
             />
         }
-        <Image
-          objectFit="cover"
-          src="images/ellipse.png"
-          position="absolute" 
-          top="32%" 
-          right="73%"
-          onClick={() => togglePosition(2)}
-          cursor={'pointer'}
-          />
-        { currentPosition.id == 2 &&
-          <Image
-            src="images/direction.png"
-            position="absolute"
-            top="34.5%"
-            right="72.5%"
-            transform={`rotate(${MathUtils.radToDeg(-rotation.y)}deg)`}
-            transformOrigin="top"
+        { currentPosition.id == 4 ?
+          <>
+            <Image
+              objectFit="cover"
+              src="images/ellipse.png"
+              position="absolute" 
+              top="79%" 
+              right="67.5%"
+              width="50px"
+              onClick={() => togglePosition(4)}
             />
+            <Image
+              src="images/direction.png"
+              position="absolute"
+              top="82%"
+              right="67.5%"
+              width="50px"
+              transform={`rotate(${MathUtils.radToDeg(-rotation.y) + 180}deg)`}
+              transformOrigin="top"
+              />
+          </>
+          :
+          <Image
+            objectFit="cover"
+            src="images/ellipse.png"
+            position="absolute" 
+            top="80%" 
+            right="67.5%"
+            onClick={() => togglePosition(4)}
+            cursor={'pointer'}
+            _hover={{ width: '50px', marginTop: '-6px', marginRight: '-6px'}}
+          />
         }
-        <Image
-          objectFit="cover"
-          src="images/ellipse.png"
-          position="absolute" 
-          top="80%" 
-          right="67.5%"
-          onClick={() => togglePosition(4)}
-          cursor={'pointer'}
-          />
-        { currentPosition.id == 4 &&
+        { currentPosition.id == 5 ?
+          <>
+            <Image
+              objectFit="cover"
+              src="images/ellipse.png"
+              position="absolute" 
+              top="79%" 
+              right="36%"
+              width="50px"
+              onClick={() => togglePosition(5)}
+              />
+            <Image
+              src="images/direction.png"
+              position="absolute"
+              top="82%"
+              right="36%"
+              width="50px"
+              transform={`rotate(${MathUtils.radToDeg(-rotation.y) + 180}deg)`}
+              transformOrigin="top"
+              />
+          </>
+          :
           <Image
-            src="images/direction.png"
-            position="absolute"
-            top="82.5%"
-            right="67%"
-            transform={`rotate(${MathUtils.radToDeg(-rotation.y) + 180}deg)`}
-            transformOrigin="top"
-            />
-        }
-        <Image
-          objectFit="cover"
-          src="images/ellipse.png"
-          position="absolute" 
-          top="80%" 
-          right="36%"
-          onClick={() => togglePosition(5)}
-          cursor={'pointer'}
+            objectFit="cover"
+            src="images/ellipse.png"
+            position="absolute" 
+            top="80%" 
+            right="36%"
+            onClick={() => togglePosition(5)}
+            cursor={'pointer'}
+            _hover={{ width: '50px', marginTop: '-6px', marginRight: '-6px'}}
           />
-        { currentPosition.id == 5 &&
-          <Image
-            src="images/direction.png"
-            position="absolute"
-            top="82.5%"
-            right="35.5%"
-            transform={`rotate(${MathUtils.radToDeg(-rotation.y) + 180}deg)`}
-            transformOrigin="top"
-            />
         }
       </Modal>
     </>
